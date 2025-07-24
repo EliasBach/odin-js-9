4068e5eb94f6cb550fb7ac391f5f807cd8fcc841
EliasBach <el.bach1999@gmail.com>
2025-07-24 20:32:21 +0200

    implement buildTree & print methods

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
-function Node(value, left, right) {
-    return {
-      value,
-      left,
-      right
-    }
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-function Tree(root) {
-    return {
-      root
-    }
-}
+class Tree {
+  constructor(arr) {
+    this.root = this.buildTree([...new Set(arr)].sort((a, b) => a - b));
+  }
 
-function buildTree(array) {
-    // remove duplicates
-    array = Array.from(new Set(array));
+  buildTree(array) {
+    if (array.length == 0) {return null};
 
-    // sort values
-    array.sort(function(a, b){return a-b})
+    const middle = Math.floor(array.length / 2);
+    const node = new Node(array[middle]);
+    node.left = this.buildTree(array.slice(0, middle));
+    node.right = this.buildTree(array.slice(middle + 1, array.length));
 
-    // build tree
-    let tree = Tree()
-    for (let entry of array) {
-        console.log(entry)
-    }
+    return node;
+  }
 
-    return tree
+  print() {
+    let node = this.root;
+    const prettyPrint = (node, prefix = "", isLeft = true) => {
+      if (node === null) {
+        return;
+      }
+      if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+      }
+      console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+      if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+      }
+    };
+    prettyPrint(node);
+  }
+
+  // NEXT TO DO: 
+  insert(value) {
+    // code
+  }
+
+  deleteItem(value) {
+    // code
+  }
 }
 
 // testing
 let test_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-buildTree(test_array)
+let testTree = new Tree(test_array)
+testTree.print()
